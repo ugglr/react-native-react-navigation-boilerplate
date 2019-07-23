180ff68d98c2a65ca919204b8b198c8ef9bde731
CarlW <carl@oskarroger.net>
2019-07-23 16:25:17 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </Fragment>
+  );
+};
+
+export default App;
